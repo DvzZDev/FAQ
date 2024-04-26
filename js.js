48fc5029c74ae7096220dac98b4da123d4dddedc
David <estebandavid578@gmail.com>
2024-04-26 17:32:19 +0200

    Update main code

--- js.js
+++ js.js
@@ -1,17 +1,25 @@
-const btns = document.getElementsByClassName("btn");
-const boxes = document.getElementsByClassName("response");
+const btns = document.querySelectorAll(".btn");
+const boxes = document.querySelectorAll(".response");
+const rotar = document.querySelectorAll(".trs");
 
 let isHide = Array.from({ length: btns.length }).fill(true);
 
-console.log(isHide);
-
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
+  btns[i].addEventListener("click", function (e) {
+    e.preventDefault();
     if (isHide[i]) {
       boxes[i].classList.add("show");
     } else {
       boxes[i].classList.remove("show");
     }
     isHide[i] = !isHide[i];
+
+    rotar.forEach(function (img, index) {
+      if (!isHide[index]) {
+        img.classList.add("rotated");
+      } else {
+        img.classList.remove("rotated");
+      }
+    });
   });
 }
